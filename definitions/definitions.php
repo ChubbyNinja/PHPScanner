fded9c8a809e25a4249e221e15a9577549110d1f
Danny Hearnah <dan.hearnah@gmail.com>
2015-04-28 16:56:32 +0100

    Installation instructions

--- definitions/definitions.php
+++ definitions/definitions.php
@@ -9,6 +9,7 @@
  * Time: 13:24
  */
 
+	// PHP definitions
 	$definitions  = [];
 	$definitions[] = '-type f -name .htpasswd';
 	$definitions[] = '-type f -name ".htpasswd"';
@@ -52,3 +53,9 @@
 	$definitions[] = 'eval(gzinflate(str_rot13(';
 	$definitions[] = '#!/usr/bin/perl';
 	$definitions[] = 'if (isset($_REQUEST[\'FILE\'])){$_FILE =$_REQUEST[';
+
+	// JS definitions
+	$definitions[ ] = '(if(window.document)s+=String["f"+"ro"+"mCh"+"arC"+ff](a[i]);)';
+	$definitions[ ] = '%uc92b%u1fb1%u0cbd%uc536%udb9b%ud9c5%u2474%u5af4%uea83%u31fc%u0b6a%u6a03%ud407%u6730%u5cff%u98bb%ud7ff%ua4fe%u9b74%uad05%u8b8b%u028d%ud893%ubccd%u35a2%u37b8%u4290%ua63a%u94e9%u9aa4%ud58d%ue5a3%u1f4c%ueb46%u4b8c%ud0ad%ua844%u524a%u3b81%ub80d%ud748%u4bd4%u6c46%u1392%u734a%u204f%uf86e%udc8e%ua207%u26b4%u04d4%ud084%uecba%u9782%u217c%ue8c0%uca8c%uf4a6%u4721%u0d2e%ua0b0%ucd2c%u00a8%ub05b%u43f4%u24e8%u7a9c%ubb85%u7dcb%ua07d%ued92%u09e1%u9631%u5580';
+	$definitions[ ] = 'eval(unescape(';
+	$definitions[ ] = 'n=\'4.5t4.5t52.5t51t16t20t50t55.5t49.5t58.5t54.5t50.5t55t58t23t51.5t50.5t58t34.5t54t50.5t54.5t50.5t55t58t57.5';
