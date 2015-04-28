3b7d8b5038b2efed5fef51a334f46ecbb5a5e506
Administrator <admin@local.host>
2015-04-28 14:08:37 +0100

    Initial commits

---
+++ definitions/definitions.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created for PHPScanner
+ * User: Danny Hearnah
+ * Author: ChubbyNinja
+ * URL: https://github.com/ChubbyNinja/PHPScanner
+ *
+ * Date: 28/04/2015
+ * Time: 13:24
+ */
+
+	$definitions  = [];
+	$definitions[] = '-type f -name .htpasswd';
+	$definitions[] = '-type f -name ".htpasswd"';
+	$definitions[] = 'netstat';
+	$definitions[] = '-type f -name "config*"';
+	$definitions[] = 'find /';
+	$definitions[] = 'ccteam.ru';
+	$definitions[] = 'c999shell';
+	$definitions[] = 'c999sh';
+	$definitions[] = 'eval(gzinflate(base64_decode(';
+	$definitions[] = 'fx29sh';
+	$definitions[] = 'psyBNC';
+	$definitions[] = 'Eggdrop';
+	$definitions[] = 'BindDoor';
+	$definitions[] = '/etc/passwd';
+	$definitions[] = 'packetstormsecurity.org';
+	$definitions[] = 'r57shell';
+	$definitions[] = 'net start';
+	$definitions[] = 'net user';
+	$definitions[] = 'net view';
+	$definitions[] = 'milw0rm';
+	$definitions[] = 'eval(base64_decode(';
+	$definitions[] = 'chandra35';
+	$definitions[] = 'g00nshell';
+	$definitions[] = 'lookup.py';
+	$definitions[] = 'Upload SUKSES';
+	$definitions[] = 'Upload GAGAL';
+	$definitions[] = 'Mr.HiTman';
+	$definitions[] = 'eval($OOO';
+	$definitions[] = 'preg_replace("/.*/e","\x65\x76\x61\x6C\x28\x67\x7A\x69\x6E';
+	$definitions[] = '9PSIpKTtldmFsKCRsbGxsbGxsbGwpOw=="));return;';
+	$definitions[] = 'pcntl_fork';
+	$definitions[] = '/bin/bash -p -i';
+	$definitions[] = 'ex("chmod 777';
+	$definitions[] = 'C102Shell';
+	$definitions[] = '$perl->eval(';
+	$definitions[] = 'fuck_you(';
+	$definitions[] = '$enc0ded';
+	$definitions[] = 'fsockopen(base64_decode(';
+	$definitions[] = '->udpflood';
+	$definitions[] = 'eval(gzinflate(str_rot13(';
+	$definitions[] = '#!/usr/bin/perl';
+	$definitions[] = 'if (isset($_REQUEST[\'FILE\'])){$_FILE =$_REQUEST[';
---
+++ scanner.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created for PHPScanner
+ * User: Danny Hearnah
+ * Author: ChubbyNinja
+ * URL: https://github.com/ChubbyNinja/PHPScanner
+ *
+ * Date: 28/04/2015
+ * Time: 13:23
+ */
+
+	require 'definitions/definitions.php';
+
+
+	function do_scan( $arr )
+	{
+		global $known_commands;
+
+
+		$found = [];
+		$content = file_get_contents( $arr['tmp_name'] );
+
+		foreach( $known_commands as $vun_id => $find )
+		{
+			if( stripos( $content, $find ) !== false )
+			{
+				$found[] = ['vun_id'=>$vun_id,'vun_string'=>$find];
+			}
+		}
+
+		$arr['scan_results'] = 'OK';
+		$arr['scan_details'] = [];
+
+		if( $found )
+		{
+			$arr['error'] = 8;
+			$arr['scan_results'] = 'PUP';
+			$arr['scan_details'] = $found;
+
+			/*
+			 * append tmp name to stop upload script continuing
+			 * we only append so the developer can decide if they
+			 * want to continue based on the scan_details array
+			 * All they require doing is removing _VIRUS_FOUND from
+			 * the tmp_name.
+			 *
+			 */
+			$arr['tmp_name'] .= '_VIRUS_FOUND';
+		}
+
+		return $arr;
+
+	}
+
+	function check_files( )
+	{
+		if( !isset($_FILES) ) {
+			return;
+		}
+
+		foreach( $_FILES as $key=>$file )
+		{
+
+			// if the upload is a single file
+			if( is_string($file['name']) )
+			{
+				$_FILES[$key] = do_scan( $file );
+			}
+
+			// if the upload is multiple files, fix php's awful array structure
+			if( is_array( $file['name'] ) )
+			{
+				foreach( $file['name'] as $file_key=> $file_name )
+				{
+					$tmp = [];
+					$tmp['tmp_name'] = $file['tmp_name'][$file_key];
+
+					$tmp = do_scan( $tmp );
+
+					foreach( $tmp as $res_key=>$res_data )
+					{
+						$_FILES[$key][$res_key][$file_key] = $res_data;
+					}
+				}
+			}
+		}
+	}
+
+	// trigger scanner
+	check_files();
