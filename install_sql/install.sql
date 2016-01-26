99baec9b35939581cdb293670777f2a2c821ee7b
Danny Hearnah <dan.hearnah@gmail.com>
2016-01-26 12:29:17 +0000

    Install SQL

--- install_sql/install.sql
+++ install_sql/install.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 19, 2016 at 04:03 PM
+-- Generation Time: Jan 26, 2016 at 12:28 PM
 -- Server version: 5.6.27-0ubuntu0.15.04.1
 -- PHP Version: 5.6.4-4ubuntu6.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `phpsc`
@@ -29,8 +23,9 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `phpsc_session` (
 `id` int(11) NOT NULL,
   `ip` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
-  `hash` varchar(50) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `hash` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -45,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `phpsc_vault` (
   `file` text COLLATE utf8_unicode_ci NOT NULL,
   `threat` text COLLATE utf8_unicode_ci NOT NULL,
   `server_details` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Indexes for dumped tables
@@ -71,12 +66,9 @@ ALTER TABLE `phpsc_vault`
 -- AUTO_INCREMENT for table `phpsc_session`
 --
 ALTER TABLE `phpsc_session`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
 -- AUTO_INCREMENT for table `phpsc_vault`
 --
 ALTER TABLE `phpsc_vault`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
