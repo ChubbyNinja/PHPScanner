1214c1f078cec96c2c53fe38154de278b3e0e1b2
Danny Hearnah <dan.hearnah@gmail.com>
2016-02-04 07:32:07 -0500

    Applied fixes from StyleCI

--- conf.custom.php
+++ conf.custom.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Created for PHPScanner
+ * User: Danny Hearnah
+ * Author: ChubbyNinja
+ * URL: https://github.com/ChubbyNinja/PHPScanner
+ */
+
 /**
  *
  * OVERWRITE DEFAULT CONFIG VARIABLES HERE
