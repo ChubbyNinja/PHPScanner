f4d5d73799ae89dd7916712697a764bd07552848
Danny Hearnah <dan.hearnah@gmail.com>
2016-02-09 17:26:30 +0000

    Changed - password field from text to password

--- webpanel/inc/login.php
+++ webpanel/inc/login.php
@@ -2,7 +2,7 @@
     <form action="?phpsc" method="post">
         <input type="hidden" name="phpsc_action" value="login">
         <label for="password">Password</label>
-        <input type="text" name="phpsc_password" value="" placeholder="">
+        <input type="password" name="phpsc_password" value="" placeholder="Type here..">
         <input type="submit" value="LOGIN">
     </form>
 </div>
